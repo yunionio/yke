4ba37dab624b37049c0220d52ff7ba2ebf3d95f0
Zexi Li <lizexi@yunionyun.com>
2018-10-29 19:45:48 +0800

    use aliyun yunionio mirror

--- pkg/types/image/mirror.go
+++ pkg/types/image/mirror.go
@@ -1,6 +1,13 @@
 package image
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	YunionMirror = "registry.cn-beijing.aliyuncs.com/yunionio"
+)
 
 var Mirrors = map[string]string{}
 
@@ -10,18 +17,19 @@ func Mirror(image string) string {
 		return image
 	}
 
-	image = strings.Replace(image, "gcr.io/google_containers", "rancher", 1)
-	image = strings.Replace(image, "quay.io/coreos/", "rancher/coreos-", 1)
+	image = strings.Replace(image, "gcr.io/google_containers", YunionMirror, 1)
+	image = strings.Replace(image, "quay.io/coreos/", fmt.Sprintf("%s/coreos-", YunionMirror), 1)
 	image = strings.Replace(image, "quay.io/calico/", "rancher/calico-", 1)
-	image = strings.Replace(image, "k8s.gcr.io/", "rancher/nginx-ingress-controller-", 1)
+	image = strings.Replace(image, "k8s.gcr.io/", fmt.Sprintf("%s/nginx-ingress-controller-", YunionMirror), 1)
 	image = strings.Replace(image, "plugins/docker", "rancher/jenkins-plugins-docker", 1)
 	image = strings.Replace(image, "kibana", "rancher/kibana", 1)
 	image = strings.Replace(image, "jenkins/", "rancher/jenkins-", 1)
 	image = strings.Replace(image, "alpine/git", "rancher/alpine-git", 1)
 	image = strings.Replace(image, "quay.io/pires", "rancher", 1)
-	image = strings.Replace(image, "quay.io/k8scsi", "registry.cn-beijing.aliyuncs.com/yunionio", 1)
-	image = strings.Replace(image, "yunion/", "registry.cn-beijing.aliyuncs.com/yunionio/", 1)
-	image = strings.Replace(image, "zexi/", "registry.cn-beijing.aliyuncs.com/yunionio/", 1)
+	image = strings.Replace(image, "quay.io/k8scsi", YunionMirror, 1)
+	image = strings.Replace(image, "yunion/", fmt.Sprintf("%s/", YunionMirror), 1)
+	image = strings.Replace(image, "zexi/", fmt.Sprintf("%s/", YunionMirror), 1)
+	image = strings.Replace(image, "rancher/", fmt.Sprintf("%s/", YunionMirror), 1)
 	Mirrors[image] = orig
 
 	return image
